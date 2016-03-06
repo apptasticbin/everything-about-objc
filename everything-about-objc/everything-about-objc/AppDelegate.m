bdf2f272225dbc2b812bd21fbfff938620c0f30e
apptasticbin <binyu.fi@gmail.com>
2016-03-06 15:48:10 +0200

    Added application life cycle experiment.

--- everything-about-objc/everything-about-objc/AppDelegate.m
+++ everything-about-objc/everything-about-objc/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import "MainViewController.h"
 #import "RootNavigationController.h"
+#import "MLog.h"
 
 @interface AppDelegate ()
 
@@ -36,6 +37,13 @@
     }
 }
 
+- (BOOL)application:(UIApplication *)application
+willFinishLaunchingWithOptions:(nullable NSDictionary *)launchOptions
+{
+    MARK;
+    return YES;
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state.
     // This can occur for certain types of temporary interruptions (such as an
@@ -43,6 +51,7 @@
     // application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down
     // OpenGL ES frame rates. Games should use this method to pause the game.
+    MARK;
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -51,18 +60,21 @@
     // application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called
     // instead of applicationWillTerminate: when the user quits.
+    MARK;
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive
     // state; here you can undo many of the changes made on entering the
     // background.
+    MARK;
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the
     // application was inactive. If the application was previously in the
     // background, optionally refresh the user interface.
+    MARK;
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
@@ -70,6 +82,7 @@
     // appropriate. See also applicationDidEnterBackground:.
     // Saves changes in the application's managed object context before the
     // application terminates.
+    MARK;
     [self saveContext];
 }
 
